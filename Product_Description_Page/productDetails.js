b6b6a046b6a7b00979d0bb24a6a6bf0a14068078
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-10-10 12:58:34 +0530

    product description page

---
+++ Product_Description_Page/productDetails.js
@@ -0,0 +1,25 @@
+
+//getElementByID
+let getElement = (id) => {
+    let tag = document.getElementById(id);
+    return tag;
+}
+
+//PC WIDTH IMAGES CLICK START
+
+let img = document.getElementById("img1").src;
+let img2 = document.getElementById("img2").src;
+let img3 = document.getElementById("img3").src;
+
+getElement("img1").onclick = () => {
+    get
+    getElement("img").src = img;
+}
+getElement("img2").onclick = () => {
+    getElement("img").src = img2;
+}
+getElement("img3").onclick = () => {
+    getElement("img").src = img3;
+}
+//PC WIDTH IMAGES CLICK END
+
