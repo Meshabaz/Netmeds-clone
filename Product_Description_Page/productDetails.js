52f1c2e6d70bd45f146aee473d35553b53e03f40
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-10-11 14:58:33 +0530

    fetching data from local storage and added page loader progress bar

--- Product_Description_Page/productDetails.js
+++ Product_Description_Page/productDetails.js
@@ -1,9 +1,56 @@
 import { description, keyBenifits, uses, information, others } from "../Product_Description_Page/description.js";
-//getElementByID
+
+
+
+//PReLoadEr
+let loader = document.getElementById("preLoader");
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+});
 let getElement = (id) => {
     let tag = document.getElementById(id);
     return tag;
 }
+
+const obj = {
+    "id": 1,
+    "prod_name": "mCaffeine Naked & Raw Tan Removal Coffee Body Scrub 100 gm",
+    "categories_1": "Personal Care",
+    "categories_2": "Bath & Shower",
+    "best_price": 377.16,
+    "mrp": 499.00,
+    "mkf": "Pep Technologies Pvt. Ltd.",
+    "Brands": "mCaffeine",
+    "discount": 16,
+    "url_1": "https://www.netmeds.com/images/product-v1/600x600/959541/mcaffeine_naked_raw_tan_removal_coffee_body_scrub_100_gm_0_0.jpg",
+    "url_2": "https://www.netmeds.com/images/product-v1/150x150/959541/mcaffeine_naked_raw_tan_removal_coffee_body_scrub_100_gm_1_0.jpg",
+    "url_3": "https://www.netmeds.com/images/product-v1/150x150/959541/mcaffeine_naked_raw_tan_removal_coffee_body_scrub_100_gm_2_0.jpg",
+    "dis": null,
+    "hsn_code": null
+}
+localStorage.setItem("clicked", JSON.stringify(obj));
+//Set DATA to Ui start
+const setData = () => {
+    const obj = JSON.parse(localStorage.getItem("clicked"));
+
+    getElement("img1").src = obj.url_1;
+    getElement("img2").src = obj.url_2;
+    getElement("img3").src = obj.url_3;
+    getElement("pName").innerText = obj.prod_name;
+    getElement("cat1").innerText = obj.categories_1;
+    getElement("cat2").innerText = obj.categories_2;
+    getElement("cimg1").src = obj.url_1;
+    getElement("cimg2").src = obj.url_2;
+    getElement("cimg3").src = obj.url_3;
+    getElement("price").innerText = obj.best_price;
+    getElement("cutPrice").innerText = obj.mrp;
+    getElement("discount").innerText = `GET ${obj.discount}% OFF`;
+    getElement("company").innerText = `* Mkt: ${obj.mkf}`;
+    getElement("default").innerText = `You get ${obj.discount}% OFF on this product`;
+}
+setData();
+//Set Data to ui end
+//getElementByID
 
 //Vertical image border click start
 function setBorder(str, id) {
@@ -18,7 +65,7 @@ setBorder("2px solid #32aeb1", "imb1");
 let img = document.getElementById("img1").src;
 let img2 = document.getElementById("img2").src;
 let img3 = document.getElementById("img3").src;
-
+getElement("img").src = img;
 getElement("img1").onclick = () => {
     getElement("img").src = img;
     setBorder("2px solid #32aeb1", "imb1");
@@ -100,4 +147,4 @@ getElement("dec5").onclick = () => {
     document.querySelector(".detailsBox").innerHTML = null;
     document.querySelector(".detailsBox").innerHTML = others();
 }
-//Description Click Start
+//Description Click End
