1768412f54e2e1c77f131638febf2fc2316f7727
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-10-13 15:25:08 +0530

    added click event on title to open details page

--- SearchedPage/search.js
+++ SearchedPage/search.js
@@ -1,30 +1,30 @@
 let searchKey = JSON.parse(localStorage.getItem("search"));
 let cartLength = JSON.parse(localStorage.getItem("cartItem"));
 
-let keyword = "lens";
+let keyword = "eye";
 let finalKey = keyword.toLowerCase();
 const searchedData = async () => {
-    let responce = await fetch("https://mr-raaz.github.io/NetmedsClone_data/data.json");
-    let data = await responce.json();
+  let responce = await fetch("https://mr-raaz.github.io/NetmedsClone_data/data.json");
+  let data = await responce.json();
 
-    showData(data.products);
+  showData(data.products);
 }
 searchedData();
 let count = 0;
 function showData(data) {
-    document.querySelector(".grid").innerHTML = null;
-    let str = "";
-    data.map(function (ele, idx) {
-        if (ele.prod_name.toLowerCase().includes(finalKey)) {
-            count++;
-            str += ` <div class="card">
+  document.querySelector(".grid").innerHTML = null;
+  let str = "";
+  data.map(function (ele, idx) {
+    if (ele.prod_name.toLowerCase().includes(finalKey)) {
+      count++;
+      str += ` <div class="card">
         <img
           src="${ele.url_1}"
           alt="img"
           id="img"
         />
         <div class="details">
-          <h1 id="title">
+          <h1 id="title" onclick="setToStorage(${idx})">
           ${ele.prod_name}
           </h1>
           <p id="category">${ele.categories_1} ${ele.categories_2}</p>
@@ -37,19 +37,31 @@ function showData(data) {
         </div>
       </div>`;
 
-            document.querySelector(".grid").innerHTML = str;
-        }
-    });
-
-    document.getElementById("totalItems").innerText = count + " " + "items found";
-
-    if (count === 0) {
-        document.querySelector(".grid").innerHTML = null;
-
-        let img = document.createElement("h1");
-        img.id = "noFound";
-        img.innerText = "No Match Found...";
-
-        document.querySelector(".grid").append(img);
+      document.querySelector(".grid").innerHTML = str;
     }
+  });
+
+  document.getElementById("totalItems").innerText = count + " " + "items found";
+
+  if (count === 0) {
+    document.querySelector(".grid").innerHTML = null;
+
+    let img = document.createElement("h1");
+    img.id = "noFound";
+    img.innerText = "No Match Found...";
+
+    document.querySelector(".grid").append(img);
+  }
+}
+
+async function setToStorage(ele) {
+  let responce = await fetch("https://mr-raaz.github.io/NetmedsClone_data/data.json");
+  let data = await responce.json();
+  console.log(data.products[ele]);
+  localStorage.setItem("clicked", JSON.stringify(data.products[ele]));
+  openDescriptionPage();
+}
+
+function openDescriptionPage() {
+  location.href = "../Product_Description_Page/productDetails.html";
 }
