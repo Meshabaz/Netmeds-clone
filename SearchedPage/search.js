b0a9efed65c3f1a5fa54d969323ac46ad3d3e42f
Md Shabaz Ansari <you@example.com>
2022-10-14 02:49:53 +0530

    Search Added

--- SearchedPage/search.js
+++ SearchedPage/search.js
@@ -1,23 +1,23 @@
 let searchKey = JSON.parse(localStorage.getItem("search"));
 let cartLength = JSON.parse(localStorage.getItem("cartItem"));
 
-let keyword = "lens";
+let keyword = "mask";
 let finalKey = keyword.toLowerCase();
 const searchedData = async () => {
-    let responce = await fetch("https://mr-raaz.github.io/NetmedsClone_data/data.json");
-    let data = await responce.json();
+  let responce = await fetch("https://mr-raaz.github.io/NetmedsClone_data/data.json");
+  let data = await responce.json();
 
-    showData(data.products);
+  showData(data.products);
 }
 searchedData();
 let count = 0;
 function showData(data) {
-    document.querySelector(".grid").innerHTML = null;
-    let str = "";
-    data.map(function (ele, idx) {
-        if (ele.prod_name.toLowerCase().includes(finalKey)) {
-            count++;
-            str += ` <div class="card">
+  document.querySelector(".grid").innerHTML = null;
+  let str = "";
+  data.map(function (ele, idx) {
+    if (ele.prod_name.toLowerCase().includes(finalKey)) {
+      count++;
+      str += ` <div class="card">
         <img
           src="${ele.url_1}"
           alt="img"
@@ -37,19 +37,19 @@ function showData(data) {
         </div>
       </div>`;
 
-            document.querySelector(".grid").innerHTML = str;
-        }
-    });
-
-    document.getElementById("totalItems").innerText = count + " " + "items found";
-
-    if (count === 0) {
-        document.querySelector(".grid").innerHTML = null;
-
-        let img = document.createElement("h1");
-        img.id = "noFound";
-        img.innerText = "No Match Found...";
-
-        document.querySelector(".grid").append(img);
+      document.querySelector(".grid").innerHTML = str;
     }
+  });
+
+  document.getElementById("totalItems").innerText = count + " " + "items found";
+
+  if (count === 0) {
+    document.querySelector(".grid").innerHTML = null;
+
+    let img = document.createElement("h1");
+    img.id = "noFound";
+    img.innerText = "No Match Found...";
+
+    document.querySelector(".grid").append(img);
+  }
 }
