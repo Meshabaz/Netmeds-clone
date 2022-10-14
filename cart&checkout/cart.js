c70476df59b29aca376ee81ef4acfe6100582908
Shiva4164 <chandragirisivaprasad1@gmail.com>
2022-10-14 23:43:16 +0530

    added total

--- cart&checkout/cart.js
+++ cart&checkout/cart.js
@@ -1,18 +1,15 @@
 var cart = JSON.parse(localStorage.getItem("cartitems")) || [];
 console.log(cart);
 displayCart(cart);
-
 function displayCart(cart) {
   var price1 = 0;
   var discount = 0;
-
   document.querySelector("#cartitems").innerHTML = "";
   document.querySelector("#MRP").innerText = "Rs." + 0;
   document.querySelector("#netmeddiscount").innerText = "Rs." + 0;
   document.querySelector("#netmed").innerText = "Rs." + 0;
   document.querySelector("#total").innerText = "Rs." + 0;
   document.querySelector("#total1").innerText = "Rs." + 0;
-
   var uniqueCartItems = [];
   var itemsQuantity = [];
 
@@ -38,64 +35,46 @@ function displayCart(cart) {
       itemsQuantity.push(1);
     }
   });
-
+  
   uniqueCartItems.forEach(function (elem, index) {
     var card = document.createElement("div");
-    card.setAttribute("class", "boxCard"); //bag12
+    card.setAttribute("class", "boxCard");
     card.style.display = "flex";
-
-    var card1 = document.createElement("div"); //img
+    var card1 = document.createElement("div");
     card1.setAttribute("class", "boxCard1");
-    // card1.style.width = "100px";
-
     var image = document.createElement("img");
     image.setAttribute("src", elem.url_1);
-
     card1.append(image);
-
-    var card2 = document.createElement("div"); //details      card2
+    var card2 = document.createElement("div");
     card2.setAttribute("class", "boxCard2");
-
-    var details = document.createElement("p"); //details p tag  card2
+    var details = document.createElement("p");
     details.innerText = elem.prod_name;
-
-    var details1 = document.createElement("p"); //  card2
+    var details1 = document.createElement("p"); 
     details1.innerText = elem.mkf;
-
-    var card3 = document.createElement("div"); //details     //card 2
+    var card3 = document.createElement("div"); 
     card3.setAttribute("class", "boxCard3");
     card3.style.display = "flex";
-
-    var card31 = document.createElement("div"); //amount      card3->31 32
+    var card31 = document.createElement("div");
     card31.setAttribute("class", "boxCard31");
-
     var price = document.createElement("span");
     price.innerText = "Rs." + elem.best_price;
-
     var Sprice = document.createElement("span");
     Sprice.innerText = "Rs." + elem.mrp;
     price1 = price1 + elem.mrp * itemsQuantity[index];
     document.querySelector("#MRP").innerText = "Rs." + price1;
-
     if (elem.mrp) {
       var discounted = elem.mrp - elem.best_price;
       discount = discount + itemsQuantity[index] * discounted;
     }
     document.querySelector("#netmeddiscount").innerText = "-Rs." + discount;
-
     document.querySelector("#netmed").innerText = "Rs." + discount;
-
     document.querySelector("#total").innerText = "Rs." + (price1 - discount);
     document.querySelector("#total1").innerText = "Rs." + (price1 - discount);
-
     var Delievery = document.createElement("p");
     Delievery.innerText = "Delivery between Oct-14 6PM - Oct-9 10PM";
-
     card31.append(price, Sprice, Delievery);
-
     var card32 = document.createElement("div");
     card32.setAttribute("class", "boxCard32");
-
     var quantityIncre = document.createElement("button");
     quantityIncre.setAttribute("class", "cc1");
     quantityIncre.innerText = "+";
@@ -108,51 +87,28 @@ function displayCart(cart) {
     quantityDecre.addEventListener("click", function () {
       delItems(elem, index);
     });
-
     var quantityValue = document.createElement("span");
-
     quantityValue.innerText = " QTY:" + itemsQuantity[index];
-    // let quantityDecre=document.createElement("div");
-    // quantityDecre.setAttribute("class", "cc2");
-    // let some=``;
-    // some+= `<div _ngcontent-pet-c6="" class="ntg">
-    //   <span _ngcontent-pet-c6="">Qty :</span>
-    //   <select _ngcontent-pet-c6="" name="quantity">
-    //     <option _ngcontent-pet-c6="" class="ng-star-inserted">1</option>
-    //     <option _ngcontent-pet-c6="" class="ng-star-inserted">2</option>
-    //     <option _ngcontent-pet-c6="" class="ng-star-inserted">3</option>
-    //     <option _ngcontent-pet-c6="" class="ng-star-inserted">4</option>
-    //   </select>
-    // </div>`;
-    // quantityDecre.innerHTML=some;
-
     var card321 = document.createElement("div");
     card321.setAttribute("class", "boxCard321");
     card321.style.display = "flex";
-
     var remove = document.createElement("button");
     remove.innerText = "REMOVE";
     remove.addEventListener("click", function () {
       removeFromCart(elem, index);
     });
-
     var later = document.createElement("button");
     later.innerText = "SAVE FOR LATER";
     later.addEventListener("click", function () {
       Saveforlater(elem, index);
     });
-
     var hr = document.createElement("hr");
     hr.setAttribute("class", "hr");
     hr.style.color = "blue";
-
     card321.append(remove, later);
-
     card32.append(quantityValue, quantityIncre, quantityDecre, card321);
-
     card3.append(card31, card32);
     card2.append(details, details1, card3, hr);
-    // card1.append(hr)
     card.append(card1, card2);
     document.querySelector("#cartitems").append(card);
   });
@@ -169,10 +125,10 @@ function delItems(elem, index) {
   for (var i = cart.length - 1; i >= 0; i--) {
     var ele = cart[i];
     if (
-      ele.img === elem.img &&
-      ele.name === elem.name &&
-      ele.price === elem.price &&
-      ele.strikedoffprice === elem.strikedoffprice
+      ele.url_1 === elem.url_1 &&
+      ele.prod_name === elem.prod_name &&
+      ele.best_price === elem.best_price &&
+      ele.mrp === elem.mrp
     ) {
       break;
     }
@@ -209,38 +165,21 @@ bbc.addEventListener("click", (event) => {
       .className.replace("fa fa-circle", "fa fa-check-circle");
   }
 });
-
-document.querySelector("#upload").addEventListener("click", myfunc);
-var a = JSON.parse(localStorage.getItem("mobile"));
-function myfunc() {
-  if (a == null) {
-    alert("Please do signup first");
-  } else {
-    window.location.href = "upload.html";
-  }
-}
-
-document.querySelector("#empty").addEventListener("click", myfunc3);
-var k = JSON.parse(localStorage.getItem("cartitems"));
-function myfunc3() {
-  if (k == null) {
-    window.location.href = "emptycart.html";
-  } else {
-    window.location.href = "cart.html";
-  }
-}
 var monkey = JSON.parse(localStorage.getItem("mobile"));
 if (monkey !== null) {
   document.querySelector("#donkey").innerText = monkey;
 }
 
 document.querySelector("#last").addEventListener("click", myfunc7);
-var k = JSON.parse(localStorage.getItem("otp"));
-function myfunc7() {
-  if (k == null) {
-    alert("You need to SignUp First");
-    window.location.href = "signUp.html";
-  } else if (k !== null) {
-    window.location.href = "cart2.html";
-  }
+function myfunc7(){
+  window.location.href="checkout.html";
 }
+// var k = JSON.parse(localStorage.getItem("otp"));
+// function myfunc7() {
+  // if (k == null) {
+  //   alert("You need to SignUp First");
+  //   window.location.href = "signUp.html";
+  // } else if (k !== null) {
+    // window.location.href = "checkout.html";
+  // }
+// }
---
+++ cart&checkout/checkout.js
@@ -0,0 +1,193 @@
+var mon = JSON.parse(localStorage.getItem("mobile"));
+if (mon !== null) {
+  document.querySelector("#don").innerText = mon;
+}
+var cart = JSON.parse(localStorage.getItem("cartitems")) || [];
+displayCart(cart);
+function displayCart(cart) {
+    var price1 = 0;
+    var discount = 0;
+    document.querySelector("#cartitems").innerHTML = "";
+    document.querySelector("#MRP").innerText = "Rs." + 0;
+    document.querySelector("#netmeddiscount").innerText = "₹" + 0;
+    document.querySelector("#netmed").innerText = "Rs." + 0;
+    document.querySelector("#total").innerText = "Rs." + 0;
+    document.querySelector("#total1").innerText = "Rs." + 0;
+    var uniqueCartItems = [];
+    var itemsQuantity = [];
+    
+    cart.forEach(function (ele) {
+        var flag = false;
+        for (var i = 0; i < uniqueCartItems.length; i++) {
+            var obj = uniqueCartItems[i];
+            if (
+                obj.url_1 === ele.url_1 &&
+                obj.prod_name === ele.prod_name &&
+                obj.best_price === ele.best_price &&
+                obj.mrp === ele.mrp &&
+                obj.mkf === ele.mkf
+                ) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                itemsQuantity[i] = itemsQuantity[i] + 1;
+            } else {
+                uniqueCartItems.push(ele);
+                itemsQuantity.push(1);
+            }
+        });
+        
+    document.querySelector("#pdone").addEventListener("click", func);
+    function func() {
+        alert("Congratulations payment done. Your product will get deliever to the mentioned address soon.");
+        window.location.href = "landing.html";
+    }
+     uniqueCartItems.forEach(function (elem, index) {
+        var card = document.createElement("div");
+        card.setAttribute("class", "boxCard");
+        card.style.display = "flex";
+
+    var card1 = document.createElement("div");
+    card1.setAttribute("class", "boxCard1");
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.url_1);
+    card1.append(image);
+    var card2 = document.createElement("div");
+    card2.setAttribute("class", "boxCard2");
+    var details = document.createElement("p"); 
+    details.innerText = elem.prod_name;
+    var details1 = document.createElement("p");
+    details1.innerText = elem.mkf;
+    var card3 = document.createElement("div");
+    card3.setAttribute("class", "boxCard3");
+    card3.style.display = "flex";
+    var card31 = document.createElement("div");
+    card31.setAttribute("class", "boxCard31");
+    var price = document.createElement("span");
+    price.innerText = "Rs." + elem.best_price;
+    var Sprice = document.createElement("span");
+    Sprice.innerText = "Rs." + elem.mrp;
+    price1 = price1 + elem.mrp * itemsQuantity[index];
+    document.querySelector("#MRP").innerText = "Rs." + price1;
+    if (elem.mrp) {
+        var discounted = elem.mrp - elem.best_price;
+        discount = discount + itemsQuantity[index] * discounted;
+    }
+    document.querySelector("#netmeddiscount").innerText = "-Rs." + discount;
+    document.querySelector("#netmed").innerText = "Rs." + discount;
+    document.querySelector("#total").innerText = "Rs." + (price1 - discount);
+    document.querySelector("#total1").innerText = "Rs." + (price1 - discount);
+    var Delievery = document.createElement("p");
+    Delievery.innerText = "Delivery between Oct-9 6PM - Oct-14 10PM";
+    card31.append(price, Sprice, Delievery);
+    var card32 = document.createElement("div");
+    card32.setAttribute("class", "boxCard32");
+    var quantityValue = document.createElement("span");
+    quantityValue.innerText = " QTY:" + itemsQuantity[index];
+    var card321 = document.createElement("div");
+    card321.setAttribute("class", "boxCard321");
+    card321.style.display = "flex";
+    var hr = document.createElement("hr");
+    hr.setAttribute("class", "hr");
+    hr.style.color = "blue";
+    card32.append(quantityValue);
+    card3.append(card31, card32);
+    card2.append(details, details1, card3, hr);
+    card.append(card1, card2);
+    document.querySelector("#cartitems").append(card);
+  });
+}
+function saved(){
+     event.preventDefault();
+     let pin=document.getElementById("pin").value;
+     let city=document.getElementById("city").value;
+     let state=document.getElementById("state").value;
+     let first=document.getElementById("first").value;
+     let last=document.getElementById("last").value;
+     let address=document.getElementById("address").value;
+     let landmark=document.getElementById("landmark").value;
+     let num=document.getElementById("num").value;
+     let details=JSON.parse(localStorage.getItem("users")) || [];
+     function Address(p,c,s,f,l,a,lm,n){
+        this.pin=p;
+        this.city=c;
+        this.state=s;
+        this.first=f;
+        this.last=l;
+        this.address=a;
+        this.landmark=lm;
+        this.num=n;
+    }
+    let det=new Address(pin, city, state, first, last, address, landmark, num);
+    details.push(det);
+    localStorage.setItem("users", JSON.stringify(details));
+}
+let details=JSON.parse(localStorage.getItem("users")) || [];
+console.log(details);
+dis(details);
+function dis(details){
+    details.map((elem)=>{
+        let first=document.createElement("h3");
+        first=elem.first;
+        let last=document.createElement("h3");
+        last=" " + elem.last;
+        let address=document.createElement("p");
+        address=elem.address;
+        let landmark=document.createElement("p");
+        landmark=elem.landmark;
+        let city=document.createElement("p");
+        city=elem.city;
+        let pin=document.createElement("p");
+        pin=" -"+elem.pin+",";
+        let state=document.createElement("p");
+        state=" "+elem.state;
+        let num=document.createElement("p");
+        num="+91- "+elem.num;
+        let box=document.createElement("div");
+        box.append(first, last);
+        let box1=document.createElement("div");
+        box1.append(city, pin, state);
+        let box2=document.createElement("div");
+        box2.append(address, landmark, box1, num);
+        let box3=document.createElement("div");
+        box3.append(box, box2);
+        box3.style.marginLeft="10px";
+        let rem=document.createElement("h4");
+        rem.innerText="remove";
+        rem.style.cursor="pointer";
+        rem.style.fontFamily="sans-serif";
+        rem.style.color="red";
+        rem.style.scrollMarginTop="-10px";
+        rem.addEventListener("click", function(){
+            removeAd(elem);
+        })
+        let box4=document.createElement("div");
+        box4.append(box3, rem);
+        box4.style.display="flex";
+        box4.style.flexDirection="row";
+        box4.style.justifyContent="space-between";
+        document.getElementById("delivery").append(box4);
+    })
+};
+function removeAd(elem) {
+    event.target.parentNode.remove();
+    var newAd = [];
+    details.forEach(function (ele) {
+        if (
+            ele.first !== elem.first||
+            ele.last !== elem.last ||
+            ele.address !== elem.address ||
+            ele.landmark !== elem.landmark ||
+            ele.city !== elem.city ||
+            ele.pin !== elem.pin ||
+            ele.state !== elem.state ||
+            ele.num !== elem.num ){
+                newAd.push(ele);
+            }
+        });
+        details = newAd;
+        localStorage.setItem("users", JSON.stringify(details));
+        displayCart(details);
+    }
