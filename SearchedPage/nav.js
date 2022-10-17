da50ae10fd4a1a1f4c4a7e1636154ac43d4d7cc8
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-10-17 08:38:04 +0530

    footer added

--- SearchedPage/nav.js
+++ SearchedPage/nav.js
@@ -1,2 +1,5 @@
 import navbar from "../scripts/navbar.js";
+import { footer } from "../scripts/footer.js";
+
+document.getElementById("footer").innerHTML = footer();
 document.getElementById("navbar").innerHTML = navbar();
